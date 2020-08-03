6ba778978f60f7ef3e453bec1a8372e60262cf3f
Edoebzd <edoardo.bosi.eb@gmail.com>
2020-08-03 21:28:41 +0200

    :pencil2: Fix typo

--- web/base_components/ErrorBoundary/ErrorBoundary.js
+++ web/base_components/ErrorBoundary/ErrorBoundary.js
@@ -11,7 +11,7 @@ export default class ErrorBoundary extends Component {
 
   static getDerivedStateFromError(error) {
     // Update state so the next render will show the fallback UI.
-    return { hasError: true };
+    return { error: true };
   }
 
   componentDidCatch(error, errorInfo) {
